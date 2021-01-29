daaa9fe4a124a8aff7c44da70d892a9d4806e532
Lian Malan <lianmalan2@gmail.com>
2021-01-29 23:17:30 +0200

    FSDA-2.13 | API | Add Connection string to appsettings and add initial EF Migration
    * Add Users table

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -3,6 +3,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
+    /// <summary>
+    /// Run "dotnet tool install --global dotnet-ef --version 5.0.2"
+    /// dotnet-ef migrations add [name] -o Data/Migrations
+    /// </summary>
     public class DataContext : DbContext
     {
         public DbSet<AppUser> Users { get; set; }
