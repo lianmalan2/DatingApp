98ec8a29a190cb000502a19b83d66aa20ca2fbb8
Lian Malan <lianmalan2@gmail.com>
2021-01-29 23:44:51 +0200

    FSDA-2.15 | API | Create Users Controller with GET endpoints

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private DataContext Context { get; }
+        public UsersController(DataContext context)
+        {
+            Context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            return Context.Users.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return Context.Users.Find(id);
+        }
+    }
+}
