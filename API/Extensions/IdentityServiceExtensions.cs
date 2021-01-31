b1d6c7339ac3b527ddb08907ba76c867d3ba262c
Lian Malan <lianmalan2@gmail.com>
2021-01-31 14:15:02 +0200

    FSDA-4.45 | API | Add extension methods for adding services in startup

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+        {
+            // Auth Stuff
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(OptionsBuilderConfigurationExtensions =>
+                {
+                    OptionsBuilderConfigurationExtensions.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                    };
+                });
+
+            return services;
+        }
+    }
+}
