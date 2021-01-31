094c970adce67eb232800e7f515ec79d752a155d
Lian Malan <lianmalan2@gmail.com>
2021-01-31 13:41:16 +0200

    FSDA-4.42 | API | Add Create Token method to Token Service

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,13 +1,42 @@
-﻿using API.Entities;
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using API.Entities;
 using API.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Services
 {
     public class TokenService : ITokenService
     {
+        public SymmetricSecurityKey Key { get; set; }
+        public TokenService(IConfiguration config)
+        {
+            Key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
         public string CreateToken(AppUser user)
         {
-            return null;
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName),
+            };
+
+            var creds = new SigningCredentials(Key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds,
+                Subject = new ClaimsIdentity(claims),
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
