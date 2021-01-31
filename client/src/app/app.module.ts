ded8c1445f2209324849c938faf993abe8c103ba
Lian Malan <lianmalan2@gmail.com>
2021-01-31 18:57:40 +0200

    FSDA-5.48 | client | Add Nav Component containing the navigation bar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
