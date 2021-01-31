5bcc96b12e891c19fb149e0a6b154cf4e6674bce
Lian Malan <lianmalan2@gmail.com>
2021-01-31 09:12:01 +0200

    FSDA-3.23 | client | Import HttpClientModule and add get users API call

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'The Dating App';
+  users$: Observable<any>;
+
+  constructor(
+    private _http: HttpClient,
+  ) { }
+
+  ngOnInit(): void {
+    this.users$ = this._http.get('https://localhost:5001/api/users').pipe(
+      catchError(err => {
+        console.log(err);
+        return [];
+      }),
+    );
+  }
 }
