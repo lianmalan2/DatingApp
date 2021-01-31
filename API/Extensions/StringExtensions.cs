a39cc7482f3afb7f134d33fdc3ff32251919d0be
Lian Malan <lianmalan2@gmail.com>
2021-01-31 12:44:13 +0200

    FSDA-4.39 | API | Add login endpoint and delete database file

---
+++ API/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace API.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool EqualsIgnoreCase(this string a, string b) =>
+            a.Equals(b, StringComparison.OrdinalIgnoreCase);
+    }
+}
