4bcc2f9619856ead3dec549bd013643aab6da6e8
Lian Malan <lianmalan2@gmail.com>
2021-01-31 20:50:11 +0200

    FSDA-5.56 | client | Cleanup property sharing between service and component

--- client/src/app/components/nav/nav.component.ts
+++ client/src/app/components/nav/nav.component.ts
@@ -1,31 +1,33 @@
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { loginInput } from 'src/app/entities/login';
 import { AccountService } from 'src/app/services/account.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
-  model: loginInput = { username: null, password: null };
-  loggedIn$: Observable<boolean> = this._accountSvc.currentUser$.pipe(
-    map((user) => !!user),
-  );
+export class NavComponent implements OnInit {
+  private initialLogin: loginInput = { username: null, password: null };
 
-  constructor(private _accountSvc: AccountService) { }
+  userLogin: loginInput = { ...this.initialLogin };
+  loggedIn$: Observable<boolean>;
+
+  constructor(
+    private _accountSvc: AccountService,
+  ) { }
+
+  ngOnInit(): void {
+    this.loggedIn$ = this._accountSvc.isLoggedIn$;
+  }
 
   login(): void {
-    this._accountSvc.login(this.model).subscribe();
+    this._accountSvc.login(this.userLogin).subscribe();
   }
 
   logout() {
     this._accountSvc.logout();
-  }
-
-  getCurrentUser() {
-    this._accountSvc.currentUser$
+    this.userLogin = { ...this.initialLogin };
   }
 }
