4e44f14ee3a2221f13d04522850c73f8a373fbfd
Lian Malan <lianmalan2@gmail.com>
2021-02-01 21:03:30 +0200

    FSDA-6.64 | client | Add routing and new components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,11 +7,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { ListsComponent } from './components/lists/lists.component';
+import { MemberDetailComponent } from './components/members/member-detail/member-detail.component';
+import { MemberListComponent } from './components/members/member-list/member-list.component';
+import { MessagesComponent } from './components/messages/messages.component';
 import { NavComponent } from './components/nav/nav.component';
 import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent,
+  ],
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
