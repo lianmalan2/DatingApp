b9a0922f3481358961b5cd398d4443d51219e6b3
Lian Malan <lianmalan2@gmail.com>
2021-02-01 22:38:23 +0200

    FSDA-7.74 | API | Create Buggy Controller to test error responses

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -7,7 +7,7 @@ namespace API.DTOs
         [Required]
         public string Username { get; set; }
         [Required]
-        [MinLength(8, ErrorMessage = "Password minimum length is 8 characters")]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
