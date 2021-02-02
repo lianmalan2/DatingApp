71bed1c7ad40ef563ebf13515a2dd383d610fa45
Lian Malan <lianmalan2@gmail.com>
2021-02-02 22:44:23 +0200

    FSDA-8.85 | API | Add Get Age method to App User class

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using API.Extensions;
 
 namespace API.Entities
 {
@@ -20,5 +21,8 @@ namespace API.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
+
+        public int GetAge() =>
+            DateOfBirth.CalculateAge();
     }
 }
