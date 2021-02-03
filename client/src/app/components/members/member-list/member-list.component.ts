4742589ae2a6a6d7db142e437f8712b9408d2e09
Lian Malan <lianmalan2@gmail.com>
2021-02-03 22:14:42 +0200

    FSDA-9.103 | client | Get list of all members and display them

--- client/src/app/components/members/member-list/member-list.component.ts
+++ client/src/app/components/members/member-list/member-list.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Member } from 'src/app/models/member';
+import { MembersService } from 'src/app/services/members.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
   styleUrls: ['./member-list.component.scss']
 })
-export class MemberListComponent {
+export class MemberListComponent implements OnInit {
+  members: Member[];
+
+  constructor(
+    private _memberSvc: MembersService,
+  ) { }
+
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this._memberSvc.getMembers().subscribe(members =>
+      this.members = members
+    );
+  }
 }
