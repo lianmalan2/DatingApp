b8d5fc533f2dfc7d87df4739d25b10e31b54afde
Lian Malan <lianmalan2@gmail.com>
2021-02-03 20:33:34 +0200

    FSDA-8.91 | API | Add User Repository

---
+++ API/Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private DataContext Context { get; }
+        public UserRepository(DataContext context)
+        {
+            Context = context;
+        }
+
+        public void Update(AppUser user)
+        {
+            Context.Entry(user).State = EntityState.Modified;
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await Context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUsersAsync()
+        {
+            return await Context.Users.ToListAsync();
+        }
+
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+            return await Context.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> getUserByUsernameAsync(string username)
+        {
+            return await Context.Users.SingleOrDefaultAsync(u => u.UserName == username);
+        }
+    }
+}
