d087a0da2e0cb90c7609a64b094c5f053f7bffd5
Lian Malan <lianmalan2@gmail.com>
2021-02-04 23:12:19 +0200

    FSDA-9.110 | client | Implement user details tabs section

--- client/src/app/modules/shared.module.ts
+++ client/src/app/modules/shared.module.ts
@@ -1,4 +1,5 @@
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ToastrModule } from 'ngx-toastr';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
@@ -11,10 +12,12 @@ import { NgModule } from '@angular/core';
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
     }),
+    TabsModule.forRoot(),
   ],
   exports: [
     BsDropdownModule,
     ToastrModule,
+    TabsModule,
   ]
 })
 export class SharedModule { }
