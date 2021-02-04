e5b564860e6b47be0f1c2df6d9c14e6a78c8d679
Lian Malan <lianmalan2@gmail.com>
2021-02-04 22:54:59 +0200

    FSDA-9.108 | client | Add View User functionality

--- client/src/app/components/members/member-detail/member-detail.component.ts
+++ client/src/app/components/members/member-detail/member-detail.component.ts
@@ -1,4 +1,8 @@
+import { Observable } from 'rxjs';
+import { Member } from 'src/app/models/member';
+import { MembersService } from 'src/app/services/members.service';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,4 +10,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./member-detail.component.scss']
 })
 export class MemberDetailComponent {
+  member$: Observable<Member> = this._memberSvc.getMember(this._route.snapshot.paramMap.get('username'));
+
+  constructor(
+    private _memberSvc: MembersService,
+    private _route: ActivatedRoute,
+  ) { }
 }
