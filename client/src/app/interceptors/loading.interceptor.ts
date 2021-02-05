a2f3a3bb1f9ee5adc47e42b3bf16105e4e254c38
Lian Malan <lianmalan2@gmail.com>
2021-02-05 22:38:24 +0200

    FSDA-10.120 | client | Add spinner to show during http requests

---
+++ client/src/app/interceptors/loading.interceptor.ts
@@ -0,0 +1,19 @@
+import { Observable } from 'rxjs';
+import { delay, finalize } from 'rxjs/operators';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BusyService } from '../services/busy.service';
+
+@Injectable()
+export class LoadingInterceptor implements HttpInterceptor {
+
+  constructor(private busySvc: BusyService) { }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    this.busySvc.busy();
+    return next.handle(request).pipe(
+      delay(500),
+      finalize(() => this.busySvc.idle()),
+    );
+  }
+}
