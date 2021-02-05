fe6005caac178fa1abec540692524951c284f387
Lian Malan <lianmalan2@gmail.com>
2021-02-05 21:45:30 +0200

    FSDA-10.117 | client | Create can deactivate guard

--- client/src/app/components/members/member-edit/member-edit.component.ts
+++ client/src/app/components/members/member-edit/member-edit.component.ts
@@ -1,12 +1,12 @@
 import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
-import { first, map, mergeMap } from 'rxjs/operators';
+import { first, mergeMap } from 'rxjs/operators';
 import { Member } from 'src/app/models/member';
 import { User } from 'src/app/models/user';
 import { AccountService } from 'src/app/services/account.service';
 import { MembersService } from 'src/app/services/members.service';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormGroup, NgForm } from '@angular/forms';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
@@ -18,45 +18,30 @@ export class MemberEditComponent implements OnInit {
 
   user$: Observable<User> = this._accountSvc.currentUser$;
   member: Member;
-  // TODO: Implement member observable in template form
-  member$: Observable<Member> = this.user$.pipe(
-    mergeMap(user => this._memberSvc.getMember(user.username)),
-  );
 
-  formGroup$: Observable<FormGroup> = this.member$.pipe(
-    map((member): FormGroup => {
-      const group: FormGroup = {} as FormGroup;
-      for (const key of Object.keys(member)) {
-        group.addControl(key, member[key])
-      }
-
-      return group;
-    })
-  );
-
+  @HostListener('window:beforeunload', ['$event']) unloadNotification($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
 
   constructor(
     private _accountSvc: AccountService,
     private _memberSvc: MembersService,
     private _toastrSvc: ToastrService,
-  ) {
-
-  }
+  ) { }
 
   ngOnInit(): void {
-    this.member$.pipe(first()).subscribe(m => this.member = m);
+    this.user$.pipe(
+      first(),
+      mergeMap(user => this._memberSvc.getMember(user.username)),
+      first(),
+    ).subscribe(m => this.member = m);
   }
 
   updateMember() {
-    // this.member$.pipe(
-    //   first(),
-    //   tap(member => console.log(member)),
-    //   tap(() => this._toastrSvc.success('Profile updated successfully')),
-    // ).subscribe(member => this.editForm.reset(member));
-
     console.log(this.member);
     this._toastrSvc.success('Profile updated successfully');
     this.editForm.reset(this.member);
   }
-
 }
--- client/src/app/guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/guards/prevent-unsaved-changes.guard.ts
@@ -1,5 +1,4 @@
-import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+
 import { Injectable } from '@angular/core';
 import { CanDeactivate } from '@angular/router';
 import { MemberEditComponent } from '../components/members/member-edit/member-edit.component';
@@ -8,12 +7,9 @@ import { MemberEditComponent } from '../components/members/member-edit/member-ed
   providedIn: 'root'
 })
 export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
-  canDeactivate(
-    component: MemberEditComponent,
-  ): Observable<boolean> {
-    return component.formGroup$.pipe(
-      filter(group => !!group),
-      map(group => group.dirty ? confirm('Are you sure you want to continue? Any unsaved changes will be lost') : true),
-    );
+  canDeactivate(component: MemberEditComponent): boolean {
+    return component.editForm.dirty
+      ? confirm('Are you sure you want to continue? Any unsaved changes will be lost')
+      : true
   }
 }
