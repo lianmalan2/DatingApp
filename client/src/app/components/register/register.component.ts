3624f95b590eaad04653f769d8105cadc0f4e6fc
lianmalan2 <lianmalan2@gmail.com>
2021-04-04 20:52:52 +0200

    FSDA-12.140 | client | Start adding reactive form group to register component

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { AccountService } from 'src/app/services';
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,15 +11,27 @@ export class RegisterComponent {
   @Output() cancelRegister = new EventEmitter();
 
   model: any = {};
+  registerForm: FormGroup;
 
-  constructor(private _accountSvc: AccountService) { }
+  constructor(private _accountSvc: AccountService) {
+    this.initializeForm();
+  }
+
+  protected initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl(),
+    })
+  }
 
   register(): void {
-    this._accountSvc.register(this.model).subscribe(user => {
-      if (!!user) {
-        this.cancel();
-      }
-    });
+    console.log(this.registerForm.value);
+    //   this._accountSvc.register(this.model).subscribe(user => {
+    //     if (!!user) {
+    //       this.cancel();
+    //     }
+    //   });
   }
 
   cancel(): void {
