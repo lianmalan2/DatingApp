acd9e28b2a42fe5de619306aa1185df8d72cef99
lianmalan2 <lianmalan2@gmail.com>
2021-04-04 20:11:25 +0200

    FSDA-11.136 | API | Create delete photo endpoint

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -104,5 +104,31 @@ namespace API.Controllers
 
             return BadRequest("Failed to set main photo");
         }
+
+        [HttpDelete("delete-photo/{photoId}")]
+        public async Task<ActionResult> DeletePhoto(int photoId)
+        {
+            var user = await UserRepo.GetUserByUsernameAsync(User.GetUsername());
+            var photo = user.Photos.FirstOrDefault(p => p.Id == photoId);
+
+            if (photo == null)
+                return NotFound();
+
+            if (photo.IsMain)
+                return BadRequest("You cannot delete your main photo");
+
+            if (photo.PublicId != null)
+            {
+                var result = await PhotoService.DeletePhotoAsync(photo.PublicId);
+                if (result.Error != null)
+                    return BadRequest(result.Error.Message);
+            }
+
+            user.Photos.Remove(photo);
+
+            if (await UserRepo.SaveAllAsync()) return Ok();
+
+            return BadRequest("Failed to delete photo");
+        }
     }
 }
