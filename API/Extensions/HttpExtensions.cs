6577e457c77626d4559779461eac006a0f46cc7f
lianmalan2 <lianmalan2@gmail.com>
2021-04-05 22:08:52 +0200

    FSDA-13.153 | API | Add pagination helper methods

---
+++ API/Extensions/HttpExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using API.Helpers;
+using Microsoft.AspNetCore.Http;
+
+namespace API.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader(
+            this HttpResponse response,
+            int currentPage,
+            int itemsPerPAge,
+            int totalItems,
+            int totalPages)
+        {
+            var header = new PaginationHeader(currentPage, itemsPerPAge, totalItems, totalPages);
+            response.Headers.Add("Pagination", JsonSerializer.Serialize(header));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
+    }
+}
