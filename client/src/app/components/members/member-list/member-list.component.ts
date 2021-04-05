46ad1097ba57dde9cffc452ac354f83873bb682a
lianmalan2 <lianmalan2@gmail.com>
2021-04-05 23:07:57 +0200

    FSDA-13.156 | client | Complete implementation of pagination in members list

--- client/src/app/components/members/member-list/member-list.component.ts
+++ client/src/app/components/members/member-list/member-list.component.ts
@@ -1,29 +1,40 @@
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Member } from 'src/app/models';
-import { Pagination } from 'src/app/models/pagination';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, mergeMap, shareReplay } from 'rxjs/operators';
 import { MembersService } from 'src/app/services';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-member-list',
   templateUrl: './member-list.component.html',
   styleUrls: ['./member-list.component.scss']
 })
-export class MemberListComponent implements OnInit {
-  members$: Observable<Member[]>;
-  pagination$: Observable<Pagination>;
-  pageNumber: number = 1;
-  pageSize: number = 5;
+export class MemberListComponent {
+  protected pageNumberSubject$: BehaviorSubject<number> = new BehaviorSubject(1);
+  protected pageNumber$: Observable<number> = this.pageNumberSubject$.asObservable().pipe(
+    distinctUntilChanged(),
+    shareReplay(1),
+  );;
+
+  protected pageSizeSubject$: BehaviorSubject<number> = new BehaviorSubject(5);
+  protected pageSize$: Observable<number> = this.pageSizeSubject$.asObservable().pipe(
+    distinctUntilChanged(),
+    shareReplay(1),
+  );
+
+  protected pagedMembers$ = combineLatest([this.pageNumber$, this.pageSize$]).pipe(
+    debounceTime(20),
+    mergeMap(([pageNumber, pageSize]) => this._memberSvc.getMembers(pageNumber, pageSize)),
+    shareReplay(1),
+  );
+
+  members$ = this.pagedMembers$.pipe(map((pm) => pm.result));
+  pagination$ = this.pagedMembers$.pipe(map((pm) => pm.pagination));
 
   constructor(
     protected _memberSvc: MembersService,
   ) { }
 
-  ngOnInit(): void {
-    const pagedMembers$ = this._memberSvc.getMembers(this.pageNumber, this.pageSize);
-
-    this.members$ = pagedMembers$.pipe(map((pm) => pm.result));
-    this.pagination$ = pagedMembers$.pipe(map((pm) => pm.pagination));
+  pageChanged(event: any) {
+    this.pageNumberSubject$.next(event.page);
   }
 }
